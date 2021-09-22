9dbc031a21e383eee4a83e34c2b9741463cf6cde
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-22 08:42:20 -0300

    teste

---
+++ GridM01/GridM01/Page2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void btVoltar(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
